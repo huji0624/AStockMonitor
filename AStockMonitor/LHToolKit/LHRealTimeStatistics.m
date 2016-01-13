ca76c84c650af9fe29e64c52b648a8422169170c
Luckyhu <huji0624@gmail.com>
2016-01-13 10:08:46 +0800

    add uuid in log.
    fyi.

--- AStockMonitor/LHToolKit/LHRealTimeStatistics.m
+++ AStockMonitor/LHToolKit/LHRealTimeStatistics.m
@@ -20,6 +20,12 @@
     NSMutableDictionary *dict = [NSMutableDictionary new];
     [dict setObject:action forKey:@"action"];
     [dict setObject:bid forKey:@"appid"];
+    
+    NSString *uuid = [self getUUID];
+    if (uuid) {
+        [dict setObject:uuid forKey:@"uuid"];
+    }
+    
     NSString *url = @"http://luckylog.sinaapp.com";
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     manager.responseSerializer = [AFHTTPResponseSerializer serializer];
@@ -28,4 +34,51 @@
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
     }];
 }
+
++ (NSString*)getUUID{
+    NSString *uuid = [self getHwUUID];
+    if (!uuid) {
+        uuid = [self getSystemSerialNumber];
+    }
+    return uuid;
+}
+
+// Hardare UUID shows in the system profiler.
++ (NSString *)getHwUUID
+{
+    CFUUIDBytes uuidBytes;
+    CFUUIDRef   uuid;
+    CFStringRef uuidString;
+    
+    gethostuuid((unsigned char *)&uuidBytes, &(const struct timespec){0, 0});
+    uuid        = CFUUIDCreateFromUUIDBytes(kCFAllocatorDefault, uuidBytes);
+    uuidString  = CFUUIDCreateString(kCFAllocatorDefault, uuid);
+    
+    if (uuid) CFRelease(uuid);
+    return CFBridgingRelease(uuidString);
+}
+
+// Hardware serial number shows in "About this Mac" window.
++ (NSString *)getSystemSerialNumber
+{
+    CFStringRef     serialNumber;
+    io_service_t    ioPlatformExpertDevice;
+    
+    serialNumber = NULL;
+    ioPlatformExpertDevice = IOServiceGetMatchingService(kIOMasterPortDefault,
+                                                         IOServiceMatching("IOPlatformExpertDevice"));
+    
+    if (ioPlatformExpertDevice) {
+        
+        CFTypeRef serialNumberCFString;
+        serialNumberCFString = IORegistryEntryCreateCFProperty(ioPlatformExpertDevice,
+                                                               CFSTR(kIOPlatformSerialNumberKey),
+                                                               kCFAllocatorDefault,
+                                                               0);
+        if (serialNumberCFString) serialNumber = serialNumberCFString;
+        IOObjectRelease(ioPlatformExpertDevice);
+    }
+    return CFBridgingRelease(serialNumber);
+}
+
 @end
